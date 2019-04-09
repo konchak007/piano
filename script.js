0905c93c8a94bb1ed8f6b2f3224213d77b7c65b6
Mariia Konchak <markonchak@gmail.com>
2019-04-09 12:51:12 +0300

    er

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-let sound= document.getElementsByTagName('audio');
-boxShadow.onclick=function () {
-    
+
+
+
+document.addEventListener('keydown', playNote);
+document.addEventListener('keyup', style);
+
+function playNote(e) {
+       const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
+       let key = document.querySelector(`button[data-key="${e.keyCode}"]`);
+       key.className=+'active-key';
+       if (!key) return;
+       audio.currentTime=0;
+       audio.play();
+}
+
+function style(e) {
+       pressedKey = document.querySelector(`button[data-key="${e.keyCode}"]`);
+       pressedKey.classList.remove('active-key');
 }
