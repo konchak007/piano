44ac736f8f17e004abe67b0017f224cbc4878ae6
Mariia Konchak <markonchak@gmail.com>
2019-04-10 11:04:09 +0300

    [add]border

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-
-
-
 document.addEventListener('keydown', playNote,style);
 document.addEventListener('click', playNote, style);
-
-
 function playNote(e) {
        const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
-       let key = document.querySelector(`button[data-key="${e.keyCode}"]`);'/'
+       let key = document.querySelector(`button[data-key="${e.keyCode}"]`);
        if (!key) return;
        audio.currentTime=0;
        audio.play();
 }
-
-function style(e) {
-       
+function style(e) {   
        pressedKey = document.querySelector(`button[data-key="${e.keyCode}"]`);
        pressedKey.classList.remove('active-key');
-
 }
