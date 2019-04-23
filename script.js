91ecf3fe916b83cfd4126688524046e1170dcaeb
Mariia Konchak <markonchak@gmail.com>
2019-04-23 14:23:33 +0300

    mistakes

--- script.js
+++ script.js
@@ -5,18 +5,21 @@ function playNote(e) {
        if (!e.keyCode) {
               dataKey = e.target.getAttribute('data-key')
        } else {
-              dataKey = e.keyCode
+              dataKey = e.keyCode;
+              
+              style(e);
        }
-       if (!dataKey) return;
        const audio = document.querySelector(`audio[data-key = "${dataKey}"]`);
        let key = document.querySelector(`button[data-key = "${dataKey}"]`);
        if (!key) return;
        audio.currentTime = 0;
        audio.play();
-      style(e);
+       
 }
 
 function style(e) {
        const pressedKey = document.querySelector(`button[data-key="${e.keyCode}"]`);
        pressedKey.classList.add('active-key');
+       setTimeout(function () { pressedKey.classList.remove('active-key') }, 100);
+       
 }
